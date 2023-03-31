cd6db523cb7024746e52bd8b96b0fddc3081dbc4
Bruno Santos <brunosantos.dev@outlook.com>
2023-03-31 22:34:30 +0200

    Add project files.

---
+++ DatabaseAccess/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseAccess.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
---
+++ DatabaseAccess/Program.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseAccess;
+
+using (var context = new DatabaseContext())
+{
+    context.Database.EnsureCreated();
+    context.Books.Add(new DatabaseAccess.Model.Book
+    {
+        Description = "Test",
+        Title = "Test",
+    });
+
+    context.SaveChanges();
+
+    var books = context.Books.ToList();
+    foreach (var book in books)
+    {
+        Console.WriteLine($"{book.Title} by {book.Description}");
+    }
+}
